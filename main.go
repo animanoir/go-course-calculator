d5ef41d4cbc4cb2c4d913f7ac786923dc3785d0d
oscaramontiel <etherealflux94@gmail.com>
2025-05-06 14:14:26 -0600

    refactors main function to calculate and display tax-inclusive prices

--- main.go
+++ main.go
@@ -1,7 +1,23 @@
 package main
 
-import "fmt"
+import (
+	// "bufio"
+	"fmt"
+)
 
 func main() {
-	fmt.Print("Hi!")
+	prices := []float64{10, 20, 30}
+	taxRates := []float64{0, 0.7, 0.1, 0.15}
+
+	result := make(map[float64][]float64) // make() creates an empty map
+	for _, taxRate := range taxRates {
+		taxIncludedPrices := make([]float64, len(prices))
+		for priceIndex, price := range prices {
+			taxIncludedPrices[priceIndex] = price * (1 + taxRate)
+		}
+		result[taxRate] = taxIncludedPrices
+	}
+	// consoleReader := bufio.NewReader(os.stdin)
+	fmt.Print(result)
+
 }
