1a4c5458e01bf863fe8ec6475bbd51b0eb131b19
oscaramontiel <etherealflux94@gmail.com>
2025-05-06 16:48:23 -0600

    implement tax calculation and data loading for tax-inclusive prices

--- main.go
+++ main.go
@@ -1,24 +1,18 @@
 package main
 
 import (
-	// "bufio"
-	"fmt"
+	"go-course-calculator/prices"
 )
 
 func main() {
-	prices := []float64{10, 20, 30}
-	taxRates := []float64{0, 0.7, 0.1, 0.15}
+	taxRates := []float64{0, 0.07, 0.1, 0.15}
 
-	result := make(map[float64][]float64) // make() creates an empty map
 	for _, taxRate := range taxRates {
-		taxIncludedPrices := make([]float64, len(prices))
-		for priceIndex, price := range prices {
-			taxIncludedPrices[priceIndex] = price * (1 + taxRate)
-		}
-		result[taxRate] = taxIncludedPrices
+		priceJob := prices.NewTaxIncludedPriceJob(taxRate)
+		priceJob.Process()
 	}
+
 	// consoleReader := bufio.NewReader(os.stdin)
-	fmt.Print(result)
 
 }
 
