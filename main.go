3ff6b295251a0e8b7db23d12e9253f48cf3ce0b7
oscaramontiel <etherealflux94@gmail.com>
2025-05-06 14:21:44 -0600

    adds personal notes to clarify data structures in code

--- main.go
+++ main.go
@@ -21,3 +21,8 @@ func main() {
 	fmt.Print(result)
 
 }
+
+//NOTE - Personal notes:
+//NOTE - Arrays are like a fixed-size egg carton. I can decide upfront how many eggs it can hold and it will never change. Each slut is numbered and can only hold one egg.
+//NOTE - Slices are like an expandable photo album. I can add more pages when needed. I can also create a "view" that shows certain pages of the album. Under the hood they are like "arrays" (references arrays), but provide flexibility to grow.
+//NOTE - A map is like a dictionary or address book. I can look up things up by a key (like a name or ID) to find the corresponding value.
